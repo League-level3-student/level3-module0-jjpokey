daec7fe69cacf361f1b0414c21295d1800cb726c
jjpokey <jasonelliott99@yahoo.com>
2020-09-28 16:52:45 -0700

    AdvancedRobotRace

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -9,19 +9,29 @@ import org.jointheleague.graphical.robot.Robot;
 public class _01_RobotRace {
 	//1. make a main method
 	public static void main(String[] args) {
-		
+		Random r = new Random();
+		int speed;
 	 
 		//2. create an array of 5 robots.
 Robot[] rob = new Robot[5];
 		//3. use a for loop to initialize the robots.
 for(int i = 0; i < rob.length; i++) {
 	rob[i] = new Robot();
+	rob[i].moveTo((i * 100 + 100), 550);
 }
 			//4. make each robot start at the bottom of the screen, side by side, facing up
-	rob[0].moveTo(10, 20);
+	
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
+for(int a = 0; a < rob.length; a++) {
+	int b = a;
+	speed = r.nextInt(51);
+	rob[a].setSpeed(speed);
+	
+	Thread t = new Thread(() -> rob[b].move(500));
+	t.start();
+}
+
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
 		//7. declare that robot the winner and throw it a party!
